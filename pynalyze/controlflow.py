aec066f20ef690ab793ba920cc4930abd412a36f
Oleg Batrashev <ogbash@gmail.com>
2010-05-18 18:38:31 +0300

    Factor some analysis code from astvis.

---
+++ pynalyze/controlflow.py
@@ -0,0 +1,250 @@
+
+class Block(object):
+    """
+    Abstract base class for blocks in Control/Data Flow analysis.
+
+    This class is a common base class for basic blocks and composite
+    types of blocks (If, Do, Case, and other blocks). It defines
+    interface of methods and common implementation which can be
+    redefined in subclasses.
+
+    @ivar model: control flow model that this block belongs to
+    @type model: L{ControlFlowModel}
+    """
+
+    def __init__(self, model, parentBlock, subBlocks = []):
+        self.model = model # control flow model
+        self.parentBlock = parentBlock
+        self.subBlocks = list(subBlocks)
+        self.firstBlock = self.subBlocks and self.subBlocks[0] or None
+        self.endBlock = None
+
+        self._nextBasicBlocks = None
+        self._previousBasicBlocks = None
+
+        self.astObjects = []
+
+    def getFirstBasicBlock(self):
+        """The first basic block that is run when entering this block.
+        @rtype: L{BasicBlock}
+        """
+        if self.firstBlock==None:
+            return None
+
+        if isinstance(self.firstBlock, BasicBlock):
+            return self.firstBlock
+        else:
+            return self.firstBlock.getFirstBasicBlock()
+
+    def getEndBlock(self):
+        """The following block after exiting this block.
+        @rtype: L{Block}
+        """
+        if self.endBlock==None and self.parentBlock!=None:
+            return self.parentBlock.getEndBlock()
+        return self.endBlock
+
+    def getNextBasicBlocks(self):
+        """Basic blocks that may be executed after exiting this block.
+        @return: list of basic blocks
+        @rtype: list of L{BasicBlock}s
+        """
+        if self._nextBasicBlocks==None:
+            nextBlock = self.getEndBlock().getFirstBasicBlock()
+            if nextBlock is not None:
+                self._nextBasicBlocks = [nextBlock]
+            else:
+                self._nextBasicBlocks = []
+        return self._nextBasicBlocks
+
+    def getPreviousBasicBlocks(self):
+        if self._previousBasicBlocks == None:
+            self.model._calculatePreviousBasicBlocks()
+        return self._previousBasicBlocks
+
+    def __str__(self):
+        return "<%s(%s)>" % (self.__class__.__name__,
+                             (self.astObjects and self.astObjects[-1] or ''))
+
+    def addSubBlocks(self, blocks):
+        self.subBlocks.extend(blocks)
+        if self.subBlocks:
+            self.firstBlock = self.subBlocks[0]
+
+    def itertree(self, callback):
+        for b in self.subBlocks:
+            b.itertree(callback)
+        callback(self)
+
+    def hasInside(self, block):
+        if block==self:
+            return True
+        if block.parentBlock!=None:
+            return self.hasInside(block.parentBlock)
+        return False
+
+class BasicBlock(Block):
+
+    def __init__(self, model, parentBlock, executions):
+        Block.__init__(self, model, parentBlock)
+        self.executions = executions
+
+        self.astObjects.extend(executions)
+
+    def getFirstBasicBlock(self):
+        return self
+
+
+class StartBlock(BasicBlock):
+    def __init__(self, model, parentBlock):
+        BasicBlock.__init__(self, model, parentBlock, [])
+
+class EndBlock(BasicBlock):
+    def __init__(self, model, parentBlock):
+        BasicBlock.__init__(self, model, parentBlock, [])
+
+    def getNextBasicBlocks(self):
+        return []
+
+class ConditionBlock(BasicBlock):
+
+    def __init__(self, model, parentBlock, executions):
+        BasicBlock.__init__(self, model, parentBlock, executions)
+        self.branchBlocks = []
+
+    def getNextBasicBlocks(self):
+        if self._nextBasicBlocks==None:
+            blocks = []
+            for branchBlock in self.branchBlocks:
+                nextBlock = branchBlock.getFirstBasicBlock()
+                if nextBlock is not None:
+                    blocks.append(nextBlock)
+            nextBlocks = super(ConditionBlock,self).getNextBasicBlocks()
+            blocks.extend(nextBlocks)
+            self._nextBasicBlocks = blocks
+        return self._nextBasicBlocks
+
+class ControlFlowModel(object):
+
+    "Model allows to navigate through AST tree of a subroutine or a program."
+
+    def __init__(self, astObj, statements):
+        self.code = astObj
+        self.block = Block(self, None)
+
+        self._startBlock = StartBlock(self, self.block)
+        self._endBlock = EndBlock(self, self.block)
+        self._codeBlock = Block(self, self.block)
+        blocks = self.generateBlocks(self._codeBlock, statements)
+        self._codeBlock.subBlocks = blocks
+        
+        self.block.subBlocks = [self._startBlock,self._codeBlock,self._endBlock]
+        self.block.firstBlock = self._startBlock
+        self._codeBlock.firstBlock = blocks[0]
+        self._startBlock.endBlock = self._codeBlock
+        self._codeBlock.endBlock = self._endBlock
+
+        self._connections = None
+
+        self._resolveJumpStatements()
+        self._allBasicBlocks = None
+
+    def generateBlocks(self, parentBlock, statements):
+        blocks = []
+        
+        simpleStatements = []
+        while len(statements)>0:
+            stmt = statements[0]
+            if stmt.__class__ in self.CLASS_MAP.keys():
+                # statement with subblocks
+                BlockClass = self.CLASS_MAP[stmt.__class__]
+                if simpleStatements:
+                    # create basic block from up-to-now simple statements
+                    blocks.append(BasicBlock(self, parentBlock, simpleStatements))
+                    simpleStatements = []
+    
+                subBlock = BlockClass(self, parentBlock, stmt)
+                blocks.append(subBlock)
+                del statements[0]
+
+            else:
+                # simple statement
+                simpleStatements.append(stmt)
+                del statements[0]
+                
+                # TODO break?? should finish simple statements and start new without breaking!!
+                if isinstance(stmt, self.JUMP_STATEMENT_CLASSES):
+                    break
+
+        if simpleStatements:
+            blocks.append(BasicBlock(self, parentBlock, simpleStatements))
+            simpleStatements = []
+
+        for i in xrange(len(blocks)-1):
+            blocks[i].endBlock = blocks[i+1]
+
+        return blocks
+
+    def _resolveJumpStatements(self):
+        pass
+
+    def _calculatePreviousBasicBlocks(self):
+        allBlocks = self.getAllBasicBlocks()
+        for block in allBlocks:
+            block._previousBasicBlocks = []
+
+        for block in allBlocks:
+            for nextBlock in block.getNextBasicBlocks():
+                nextBlock._previousBasicBlocks.append(block)
+
+    def getAllBasicBlocks(self):
+        if self._allBasicBlocks==None:            
+            blocks = set()
+
+            def collect(block):
+                if isinstance(block, BasicBlock):
+                    blocks.add(block)
+
+            self.block.itertree(collect)
+            self._allBasicBlocks = blocks
+
+        return self._allBasicBlocks
+
+    def getConnections(self):
+        if self._connections==None:
+            connections = set()
+            processed = set()
+            blocks = set()
+            blocks.add(self.block.getFirstBasicBlock())
+            while blocks:
+                block = blocks.pop()
+                processed.add(block)
+                nextBlocks = block.getNextBasicBlocks()
+                for nextBlock in nextBlocks:
+                    connections.add((block, nextBlock))
+                    if nextBlock!=None and not nextBlock in processed:
+                        blocks.add(nextBlock)
+            self._connections = connections
+        return self._connections
+
+    def classifyConnectionsBy(self, connections, blocks):
+        """Takes list of tuples and divides them into classes, where
+        each class is some connection for I{blocks} and its elements
+        are all connections of the subBlocks of I{blocks}.
+        """
+        blockSet = set(blocks)
+
+        def find(block):
+            while not block in blockSet and block!=None:
+                block = block.parentBlock
+            return block
+
+        clConnections = {}
+        for blockFrom, blockTo in connections:
+            clBlockFrom = find(blockFrom)
+            clBlockTo = find(blockTo)
+            if not clConnections.has_key((clBlockFrom,clBlockTo)):
+                clConnections[(clBlockFrom,clBlockTo)] = []
+            clConnections[(clBlockFrom,clBlockTo)].append((blockFrom,blockTo))
+
+        return clConnections
---
+++ pynalyze/dataflow.py
@@ -0,0 +1,181 @@
+
+class Dict(object):
+    def __init__(self, d=None):
+        if d!=None:
+            self._values = self._copyDict(d._values)
+        else:
+            self._values = {}            
+
+    def _copyDict(self, d):
+        v = {}
+        for key in d.keys():
+            if isinstance(d[key], dict):
+                v[key] = self._copyDict(d[key])
+            else:
+                v[key] = d[key]
+        return v
+
+    def __getitem__(self, key):
+        d = self._values
+        for name in key:
+            if not d.has_key(name):
+                return None
+            d = d[name]
+
+        return d.get(None, None)
+
+    def __setitem__(self, key, value):
+        d = self._values
+        for name in key:
+            if not d.has_key(name):
+                d[name]={}
+            d = d[name]
+        d[None] = value
+
+    def add(self, key, loc):
+        d = self._values
+        for name in key:
+            if not d.has_key(name):
+                d[name]={}
+            d = d[name]
+        if not d.has_key(None):
+            d[None] = set()
+        d[None].add(loc)
+
+    def remove(self, key):
+        d = self._values
+        for name in key:
+            if not d.has_key(name):
+                return False
+            p = d
+            d = d[name]
+        del p[name]
+
+    def _update(self, toDict, fromDict):
+        for name in fromDict.keys():
+            if name is None:
+                # value
+                if not toDict.has_key(name):
+                    toDict[name] = set()
+                toDict[name].update(fromDict[name])
+            else:
+                # subcomponent with name
+                if not toDict.has_key(name):
+                    toDict[name] = dict()
+                self._update(toDict[name], fromDict[name])
+
+    def update(self, fromDict):
+        self._update(self._values, fromDict._values)
+
+    def __eq__(self, d):
+        if d is None:
+            return False
+        return self._values==d._values
+
+    def __ne__(self, d):
+        if d is None:
+            return True
+        return self._values!=d._values
+
+    def keys(self):
+        k = set()
+        self._keys(self._values, tuple(), k)
+        return k
+
+    def _keys(self, d, prefix, k):
+        for name in d.keys():
+            if name is None:
+                k.add(prefix)
+            else:
+                self._keys(d[name], prefix+(name,), k)
+
+    def intersection(self, otherDict):
+        pairs = []
+        self._intersect(self._values, otherDict._values, pairs)
+        return pairs
+
+    def __collectValues(self, d, values):
+        if d.has_key(None):
+            values.update(d[None])
+        for key in d.keys():
+            if key is not None:
+                self.__collectValues(d[key], values)
+
+    def _intersect(self, d1, d2, pairs):
+        commonNames = set(d1.keys()).intersection(set(d2.keys()))
+        
+        for name in commonNames:
+            if name is not None:
+                # take definition and subcomponent references
+                if d1[name].has_key(None):
+                    locs2 = set()
+                    self.__collectValues(d2[name], locs2)
+                    for loc1 in d1[name][None]:
+                        for loc2 in locs2:
+                            pairs.append((loc1,loc2))
+
+                # take reference and subcomponent definitions
+                if d2[name].has_key(None):
+                    locs1 = set()
+                    self.__collectValues(d1[name], locs1)
+                    for loc2 in d2[name][None]:
+                        for loc1 in locs1:
+                            pairs.append((loc1,loc2))
+
+                # recurse
+                self._intersect(d1[name], d2[name], pairs)
+
+class LiveVariableDict(Dict):
+    pass
+
+class ReachingDefinitionDict(Dict):
+    pass
+
+class ReachingDefinitions(object):
+    
+    def __init__(self, controlFlowModel):
+        self.controlFlowModel = controlFlowModel
+        self.ins, self.outs = self._analyze()
+
+    def _analyze(self):
+        ins = {} # { block: {name: set(flow.ASTLocation)}}
+        outs = {} # { block: {name: set(flow.ASTLocation)}}
+
+        # some help functions
+        def IN(block):
+            if not ins.has_key(block):
+                ins[block] = ReachingDefinitionDict()
+            return ins[block]
+
+        def OUT(block, createNew=True):
+            if not outs.has_key(block):
+                if not createNew:
+                    return None
+                outs[block] = ReachingDefinitionDict()
+            return outs[block]
+
+        # start main loop which works until converging of IN/OUT states
+        
+        working = [] # blocks that have their IN redefined
+        working.append(self.controlFlowModel._startBlock)
+
+        while working:
+            block = working.pop()
+            inDefs = IN(block)
+            oldOutDefs = OUT(block, False)
+            outDefs = self._transform(inDefs, block)
+            changed = (outDefs != oldOutDefs)
+            if changed:
+                outs[block] = outDefs
+                # OUT has changed since the last try
+                #  add following basic blocks to the working set as their INs change
+                for nextBlock in block.getNextBasicBlocks():
+                    if nextBlock==None:
+                        # @todo: fix parser (handle "read" statement)
+                        # this is only necessary, because fortran parser is not complete
+                        #  i.e. there may exist empty block
+                        continue 
+                    nextInDefs = IN(nextBlock)
+                    nextInDefs.update(outDefs)
+                    working.append(nextBlock)
+        return ins, outs
